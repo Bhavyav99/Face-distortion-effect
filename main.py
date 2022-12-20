3504545cdfe32163532ee6b88da5c5a1f760cd63
osamaalschameri <74721369+osamaalschameri@users.noreply.github.com>
2022-12-20 10:20:21 +0100

    video is successfully generated

--- main.py
+++ main.py
@@ -1,25 +1,48 @@
 import os
-
+import  numpy as np
 import cv2
+import random
 images=[]
-for roots,dirs,files in os.walk('/images'):
+for roots,dirs,files in os.walk('/Users/osama-mac/Downloads/img_align_celeba'):
     for file in files:
         if file.endswith('.jpg'):
             images.append(os.path.join(roots,file))
 #
-mage_folder = 'images'
-video_name = 'video.avi'
-each_image_duration = 5 # in secs
-fourcc =  cv2.VideoWriter_fourcc(*'DIVX')
+
+video_name = 'video.mp4'
+each_image_duration = 2 # in secs
+fourcc =  cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
 
 frame = cv2.imread(images[0])
 height, width, layers = frame.shape
 print(height,width)
-video = cv2.VideoWriter(video_name, fourcc, 1.0, (width, height))
+cross_image=cv2.resize(cv2.imread('cross.jpg'),(width,height),interpolation=cv2.INTER_NEAREST)
+video = cv2.VideoWriter(video_name, fourcc, 1.0, (534, 218))
+choosen_images=[]
+for img  in range(0,20):
+    selected_image1=None
+    selected_image2=None
 
-for image in images:
+    while images:
+        selected_image1 = random.choice(images)
+        if selected_image1 not in choosen_images:
+            choosen_images.append(selected_image1)
+            break
+
+    while images:
+        selected_image2 = random.choice(images)
+        if selected_image2 not in choosen_images:
+            choosen_images.append(selected_image2)
+            break
+
+
+    print(choosen_images.__len__())
+    concated_images = np.concatenate([
+        cv2.imread(selected_image1), cross_image, cv2.imread(selected_image2)
+        ], axis=1)
+
     for _ in range(each_image_duration):
-        video.write(cv2.imread(image))
+        video.write(concated_images)
 
 # cv2.destroyAllWindows()
 video.release()
