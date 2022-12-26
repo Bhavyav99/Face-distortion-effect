6565937ce0e607fa506bc232c1e726ba5180d6f5
Bhavyavreddy <bhavya.venkata@carealytix.com>
2022-12-26 14:07:29 +0100

    added python file with video

---
+++ display_trial.py
@@ -0,0 +1,61 @@
+from PIL import Image
+import cv2
+import os
+import  numpy as np
+import cv2
+import random
+from numpy import asarray
+
+
+images=[]
+for roots,dirs,files in os.walk('C:/Users/resu/Desktop/celeba/img_align_celeba'):
+    for file in files:
+        if file.endswith('.jpg'):
+            images.append(os.path.join(roots,file))
+
+
+video_name = 'mygeneratedvideo.avi'
+fourcc = cv2.VideoWriter_fourcc(*'DIVX')
+
+
+video = cv2.VideoWriter(video_name, fourcc, 0.5, (1846,926))
+
+choosen_images=[]
+
+img1 = Image.open("C:/Users/resu/Desktop/CVP Project/Face-distortion-effect/cross_background.png")
+print(img1.size)
+for img  in range(0,20):
+    selected_image1=None
+    selected_image2=None
+
+    while images:
+        selected_image1 = random.choice(images)
+        if selected_image1 not in choosen_images:
+            choosen_images.append(selected_image1)
+            break
+
+    while images:
+        selected_image2 = random.choice(images)
+        if selected_image2 not in choosen_images:
+            choosen_images.append(selected_image2)
+            break
+
+
+    img1 = Image.open("C:/Users/resu/Desktop/CVP Project/Face-distortion-effect/cross_background.png")
+    img2 = Image.open(selected_image1)
+    img3 = Image.open(selected_image2)
+
+    new_width = 400
+    new_height = 500
+    img2 = img2.resize((new_width, new_height), Image.ANTIALIAS)
+    img3 = img3.resize((new_width, new_height), Image.ANTIALIAS)
+
+    img1.paste(img2, (200, 200))
+    img1.paste(img3, (1250, 200))
+    numpydata = np.asarray(img1)
+    #img1.show()
+
+    video.write(cv2.cvtColor(np.array(numpydata), cv2.COLOR_RGB2BGR))
+
+video.release()
+
